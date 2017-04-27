3066c04026388acd4c25ee4491eb83e32fc3bb61
hemasara <hema.m5@gmail.com>
2017-04-27 11:28:47 +0530

    Add files via upload

--- HathwayLogistic/src/main/java/com/hema/logistics/configuration/MvcConfiguration.java
+++ HathwayLogistic/src/main/java/com/hema/logistics/configuration/MvcConfiguration.java
@@ -4,13 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.hema.logistics")
 
-public class MvcConfiguration {
+public class MvcConfiguration extends WebMvcConfigurerAdapter{
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
 
 	@Bean(name="viewResolver")	
 	public InternalResourceViewResolver viewResolver(){
